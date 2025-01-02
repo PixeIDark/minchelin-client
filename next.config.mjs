cac1ad43746b49a322d2e6ce2b9522fb20c3779c
Darkthos <kiki7951@naver.com>
2025-01-02 12:07:54 +0900

    cloudinary 도메인 허가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    middlewareTracing: true, // 미들웨어 디버깅 옵션
+  images: {
+    domains: ['res.cloudinary.com'],
   },
 };
 
