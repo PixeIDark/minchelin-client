761f8d4cfa357b9b159e69f1adbb213bc19a8239
Darkthos <kiki7951@naver.com>
2025-01-03 18:17:38 +0900

    페이지별 헤더 존재 유무 조정

--- src/app/_components/layout/header-layout/HeaderLayout.tsx
+++ src/app/_components/layout/header-layout/HeaderLayout.tsx
@@ -3,9 +3,16 @@
 import { useSearchPanel } from '@/app/_components/layout/header-layout/_hooks/useSearchPanel';
 import { Header } from '@/app/_components/header';
 import { SearchPanel } from '@/app/_components/search-panel';
+import { useSelectedLayoutSegment } from 'next/navigation';
+import { EXCLUDED_SEGMENTS } from '@/app/_components/layout/header-layout/constants/excludedSegments';
+
+const excludedSegments = new Set(EXCLUDED_SEGMENTS);
 
 function HeaderLayout() {
   const { isSearchOpen, onSearchToggle } = useSearchPanel();
+  const segment = useSelectedLayoutSegment();
+
+  if (segment && excludedSegments.has(segment)) return null;
 
   return (
     <div>
