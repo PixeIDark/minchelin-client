a3265ddeb17bd80f25eab7ab767d364208112c22
Darkthos <kiki7951@naver.com>
2025-01-18 02:24:14 +0900

    Picture.tsx 범용성 상향

--- src/components/common/picture/Picture.tsx
+++ src/components/common/picture/Picture.tsx
@@ -3,14 +3,33 @@ import React from 'react';
 interface PictureProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   src: string;
   fallbackSrc?: string;
+  width?: number;
+  height?: number;
 }
 
-function Picture({ src, alt, fallbackSrc = '/placeholder.png', ...props }: PictureProps) {
+function Picture({
+  src,
+  alt,
+  fallbackSrc = '/placeholder.png',
+  width,
+  height,
+  ...props
+}: PictureProps) {
   const handleError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     e.currentTarget.src = fallbackSrc;
   };
 
-  return <img src={src} alt={alt} onError={handleError} loading='lazy' {...props} />;
+  return (
+    <img
+      src={src}
+      alt={alt}
+      onError={handleError}
+      loading='lazy'
+      width={width}
+      height={height}
+      {...props}
+    />
+  );
 }
 
 export default Picture;
