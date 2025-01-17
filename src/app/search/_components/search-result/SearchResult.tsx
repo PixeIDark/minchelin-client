f2e354dfb102c70686d1864d6e85f67641b0f788
Darkthos <kiki7951@naver.com>
2025-01-18 00:18:38 +0900

    SearchResult.tsx 로직, 컴포넌트 분리

--- src/app/search/_components/search-result/SearchResult.tsx
+++ src/app/search/_components/search-result/SearchResult.tsx
@@ -1,58 +1,23 @@
 'use client';
 
-import { useCallback, useRef } from 'react';
-import { useSearch } from '@/queries/search';
 import { SearchList } from './search-list';
 import { SearchPanel } from './search-panel';
-import { useIntersection } from '@/hooks/useIntersection';
 import { css } from '@/styled-system/css';
 import { SearchParams } from '@/types/search';
+import { useSearchResult } from '@/app/search/_components/search-result/hooks/useSearchResult';
+import { Loader2 } from 'lucide-react';
+import React from 'react';
+import { LoadingSpinner } from '@/components/common/loading-spinner';
 
 function SearchResult({ searchParams }: { searchParams: SearchParams }) {
-  const loaderRef = useRef<HTMLDivElement>(null);
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useSearch(searchParams);
-
-  const handleIntersect = useCallback(() => {
-    if (hasNextPage && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
-
-  useIntersection(loaderRef, { onIntersect: handleIntersect, rootMargin: '100px' });
-
-  const songs = data?.pages.flatMap((page) => page.items) ?? [];
-  const totalSongs = data?.pages[0]?.total ?? 0;
+  const { songs, totalSongs, loaderRef, isLoading } = useSearchResult(searchParams);
 
   return (
-    <div
-      className={css({
-        h: 'full',
-        display: 'flex',
-        flexDir: 'column',
-      })}
-    >
+    <div>
       <SearchPanel totalSongs={totalSongs} />
-      <div
-        className={css({
-          flex: 1,
-          overflowY: 'auto',
-          '&::-webkit-scrollbar': { display: 'none' },
-        })}
-      >
-        <SearchList searchList={songs} />
-        <div ref={loaderRef} className={css({ h: '10px', mt: '4' })} />
-        {isFetchingNextPage && (
-          <div
-            className={css({
-              textAlign: 'center',
-              py: '4',
-              color: 'gray.500',
-            })}
-          >
-            로딩중...
-          </div>
-        )}
-      </div>
+      <SearchList searchList={songs} />
+      <div ref={loaderRef} className={css({ h: '10px', mt: '4' })} />
+      <LoadingSpinner isLoading={isLoading}>노래 목록을 가져오는 중</LoadingSpinner>
     </div>
   );
 }
