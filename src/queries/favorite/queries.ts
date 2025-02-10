d7fb254c44e01a4e53ceaaa710f16c48c964495f
Darkthos <kiki7951@naver.com>
2025-02-10 17:56:47 +0900

    favorite 쿼리 범용성 상향

--- src/queries/favorite/queries.ts
+++ src/queries/favorite/queries.ts
@@ -1,18 +1,26 @@
 // 즐겨찾기 목록 조회
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { favoritesApi } from '@/api/favorites';
 import { FAVORITE_KEYS } from '@/queries/favorite/key';
+import { GetFavoriteListSongsResponse, GetFavoriteListsResponse } from '@/types/favorite';
 
-export const useFavoriteLists = () => {
+export const useFavoriteLists = (
+  options?: Omit<UseQueryOptions<GetFavoriteListsResponse, Error>, 'queryKey' | 'queryFn'>
+) => {
   return useQuery({
     queryKey: FAVORITE_KEYS.lists(),
     queryFn: () => favoritesApi.getLists(),
+    ...options,
   });
 };
 
-export const useFavoriteListSongs = (listId: number) => {
+export const useFavoriteListSongs = (
+  listId: number,
+  options?: Omit<UseQueryOptions<GetFavoriteListSongsResponse, Error>, 'queryKey' | 'queryFn'>
+) => {
   return useQuery({
     queryKey: FAVORITE_KEYS.songs(listId),
     queryFn: () => favoritesApi.getListSongs(listId),
+    ...options,
   });
 };
