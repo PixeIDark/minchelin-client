badda56def86c2f0bef216c06ac93b7e3a615adb
Darkthos <kiki7951@naver.com>
2025-02-10 17:56:15 +0900

    accordion 열닫 키 프레임 추가로 해결

--- panda.config.ts
+++ panda.config.ts
@@ -15,6 +15,16 @@ export default defineConfig({
       tokens: {
         colors,
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
     },
   },
 
